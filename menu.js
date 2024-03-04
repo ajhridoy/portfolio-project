5bbe02690f9f91a025380d2b3518d22c296c111f
ajhridoy <ajhridoy461@gmail.com>
2024-03-05 02:36:47 +0600

    toggle menubar updated

---
+++ menu.js
@@ -0,0 +1,14 @@
+// menu-bar
+$(".menu-toggle").on('click', function() {
+    $(this).toggleClass("on");
+    $('.menu-section').toggleClass("on");
+    $("nav ul").toggleClass('hidden');
+  });
+  
+  $(".list").on('click', function() {
+    $(this).toggleClass("on");
+    $('.menu-section').toggleClass("on");
+    $('.menu-toggle').toggleClass("on");
+    $("nav ul").toggleClass('hidden');
+    
+  });
